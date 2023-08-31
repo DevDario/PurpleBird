39becc0c7c4a82d6807501fd8fd56f9f2975c57d
Dário Silva <dariosilva13222@gmail.com>
2023-08-31 15:09:01 +0000

    🚀FEAT: added a link to icon8s

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <a  href="https://icons8.com/icon/VJd4TEEr0kDo/bird">bird</a> icon by <a href="https://icons8.com">Icons8</a>
+    </div>
+  )
+}
+
+export default Footer
