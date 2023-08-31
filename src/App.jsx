2f0319569860cbd671325a3f946a60cb862b0285
Dário Silva <dariosilva13222@gmail.com>
2023-08-31 23:59:01 +0000

    🚀FEAT> imported Image component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navbar from './layout/Navbar'
 import Footer from './layout/Footer'
+import Image from './utils/Image/Image'
+import ReceptorImage from './assets/businessman-working-laptop.jpg'
 
 function App() {
 
   return (
-    <>
+    <div className='app'>
       <Navbar/>
 
       <h1 className='homeTitle'>
@@ -14,8 +16,12 @@ function App() {
         browser tool !
       </h1>
 
+      <Image 
+        imageSource={ReceptorImage}
+      />
+
       <Footer/>
-    </>
+    </div>
   )
 }
 
