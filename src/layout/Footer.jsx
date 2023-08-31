eef883ac49845929a447b6facd6b6a543de0371c
Dário Silva <dariosilva13222@gmail.com>
2023-08-31 15:56:41 +0000

    🚀FEAT:added more informations

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import style from './Footer.module.css'
+import Logo from '../utils/Logo/Logo'
 
 const Footer = () => {
   return (
-    <div>
-        <a  href="https://icons8.com/icon/VJd4TEEr0kDo/bird">bird</a> icon by <a href="https://icons8.com">Icons8</a>
-    </div>
+    <footer className={style.footerContainer}>
+      <div className={style.about}>
+          
+          <Logo 
+            fontColor={"#fcfcfb"}
+          />
+           <p className={style.aboutDescription}>
+            PurpleBird is a free open source e-mail sender.<br/>
+            with PurpleBird you can send faster emails using the <br/>
+            MailGun API to send them, so you can have a better <br/>
+            experience and faster results.
+           </p>
+
+      </div>
+        
+        <div className={style.iconsSource}>
+          <a  href="https://icons8.com/icon/VJd4TEEr0kDo/bird">bird</a> 
+          icon by <a href="https://icons8.com">Icons8</a>
+        </div>
+
+        <div className={style.credits}>
+            <p>Made With <span className={style.hearth}>❤</span> By Dário Silva</p>
+        </div>
+
+    </footer>
   )
 }
 
