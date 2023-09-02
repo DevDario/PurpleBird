477c64d5ccfb7a144cbee8e33a6be1d04ce5c9c9
Dário Silva <dariosilva13222@gmail.com>
2023-09-02 11:02:52 +0000

    🚀FEAT: feature section component

---
+++ src/utils/Feature/Feature.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import style from './Feature.module.css'
+import PropTypes from 'prop-types'
+
+const Feature = ({title, description, icon, circleWidth}) => {
+  return (
+    <section className={style.featureContainer}>
+        <div className={style.circle} style={{
+            width: circleWidth,
+            height: circleWidth
+        }}>
+           {icon}
+        </div>
+        <h2 className={style.title}>{title}</h2>
+        <p className={style.description} style={{
+            textAlign:"center",
+            fontWeight: "200",
+            flexWrap:"wrap"
+        }}>{description}</p>
+    </section>
+  )
+}
+
+Feature.propTypes = {
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    icon: PropTypes.elementType.isRequired,
+    circleWidth: PropTypes.string.isRequired,
+}
+
+Feature.defaultProps = {
+    title: "it's easy",
+    description: "PurpleBird it's easy to use and very acessible for everyone. We had the ideia of creating something useful, intuitive and very user-frienfly so everyone can enjoy it as much as us",
+    circleWidth: "90px"
+}
+
+export default Feature
