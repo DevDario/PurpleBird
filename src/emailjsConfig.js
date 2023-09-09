0dcf402011c4a1a4babbab20d4772ff05a8f9017
Dário Silva <dariosilva13222@gmail.com>
2023-09-09 17:31:19 +0000

    👨‍💻CHORE: emailjs account data

---
+++ src/emailjsConfig.js
@@ -0,0 +1,8 @@
+const emailjsConfig = {
+    serviceID: import.meta.env.VITE_EMAILJS_SERVICE_ID,
+    templateID: import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+    userID: import.meta.env.VITE_EMAILJS_USER_ID,
+  };
+  
+  export default emailjsConfig;
+  
