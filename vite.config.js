824ff97aee760fca8b5bc566508ab16fc4c1547f
Dário Silva <dariosilva13222@gmail.com>
2023-09-09 17:30:35 +0000

    👨‍💻CHORE: defined environment variables

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define:{
+    'process.env.VITE_EMAILJS_SERVICE_ID': JSON.stringify(process.env.VITE_EMAILJS_SERVICE_ID),
+    'process.env.VITE_EMAILJS_TEMPLATE_ID': JSON.stringify(process.env.VITE_EMAILJS_TEMPLATE_ID),
+    'process.env.VITE_EMAILJS_USER_ID': JSON.stringify(process.env.VITE_EMAILJS_USER_ID)
+  }
 })
