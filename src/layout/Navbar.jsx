a1a17dce06970dea6ec096bca2a3c8363a7106e9
Dário Silva <dariosilva13222@gmail.com>
2023-09-13 16:50:22 +0000

    🔄REFACTOR: change to direct route path

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
           {
             <img width="48" height="48" alt="Purple Bird" src="https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/48/7950F2/external-bird-origami-tanah-basah-glyph-tanah-basah.png"/> && (
 
-              <img width="48" height="48" alt="Purple Bird" src="/public/favicon.png"/>
+              <img width="48" height="48" alt="Purple Bird" src="/favicon.png"/>
             )
 
           }
